ebd6d7f4681631bdfff2d6c892551d2128264e89
vigneshran <rangaraj.vignesh@ou.edu>
2017-06-04 01:49:51 -0400

    Update borrower-constants.js

--- src/main/webapp/resources/business/Borrower/borrower-constants.js
+++ src/main/webapp/resources/business/Borrower/borrower-constants.js
@@ -2,10 +2,10 @@ lmsApp
 		.constant(
 				"basicBorrowerConstants",
 				{
-					ACCESS_CHECK_URL : "http://localhost:8080/lms/borrower/doesNumberExist/",
-					GET_BORROWER_BY_PK_URL : "http://localhost:8080/lms/borrower/returnBorrowerByCardNoBasedOnLoans/",
-					GET_ALL_BRANCHES_URL : "http://localhost:8080/lms/borrower/readAllBranches",
-					GET_INIT_LOAN_URL : "http://localhost:8080/lms/borrower/getInitLoan/",
-					CHECK_OUT_BOOK_URL : "http://localhost:8080/lms/borrower/addLoan",
-					RETURN_BOOK_URL : "http://localhost:8080/lms/borrower/updateLoan"
+					ACCESS_CHECK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/borrower/doesNumberExist/",
+					GET_BORROWER_BY_PK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/borrower/returnBorrowerByCardNoBasedOnLoans/",
+					GET_ALL_BRANCHES_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/borrower/readAllBranches",
+					GET_INIT_LOAN_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/bborrower/getInitLoan/",
+					CHECK_OUT_BOOK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/borrower/addLoan",
+					RETURN_BOOK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/borrower/updateLoan"
 				})
