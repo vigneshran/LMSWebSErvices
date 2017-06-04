6cb2de6ea1408d867bb8663aac48f0945813a304
vigneshran <rangaraj.vignesh@ou.edu>
2017-06-04 01:19:00 -0400

    Update borrower-constants.js

--- src/main/webapp/resources/business/AdminBorrower/borrower-constants.js
+++ src/main/webapp/resources/business/AdminBorrower/borrower-constants.js
@@ -2,12 +2,12 @@ lmsApp
 		.constant(
 				"borrowerConstants",
 				{
-					GET_ALL_BORROWERS_URL : "http://localhost:8080/lms/admin/readAllBorrowers",
-					GET_BORROWER_BY_PK_URL : "http://localhost:8080/lms/admin/returnBorrowerByPK/",
-					GET_BORROWER_BY_NAME_URL : "http://localhost:8080/lms/admin/returnBorrowerByName/",
-					GET_BORROWER_INIT_URL : "http://localhost:8080/lms/admin/getInitBorrower",
-					ADD_BORROWER_URL : "http://localhost:8080/lms/admin/addBorrower",
-					UPDATE_BORROWER_URL : "http://localhost:8080/lms/admin/updateBorrower",
-					DELETE_BORROWER_URL : "http://localhost:8080/lms/admin/deleteBorrower",
-					SEARCH_BORROWERS_URL : "http://localhost:8080/lms/admin/searchByBorrowerNameOnly/"
+					GET_ALL_BORROWERS_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/readAllBorrowers",
+					GET_BORROWER_BY_PK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/returnBorrowerByPK/",
+					GET_BORROWER_BY_NAME_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/returnBorrowerByName/",
+					GET_BORROWER_INIT_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/getInitBorrower",
+					ADD_BORROWER_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/addBorrower",
+					UPDATE_BORROWER_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/updateBorrower",
+					DELETE_BORROWER_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/deleteBorrower",
+					SEARCH_BORROWERS_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/searchByBorrowerNameOnly/"
 				})
