2e649959c13a083fa72dcf24d5fe1116837b438b
vigneshran <rangaraj.vignesh@ou.edu>
2017-06-04 01:16:28 -0400

    Update author-constants.js

--- src/main/webapp/resources/business/AdminAuthor/author-constants.js
+++ src/main/webapp/resources/business/AdminAuthor/author-constants.js
@@ -2,11 +2,11 @@ lmsApp
 		.constant(
 				"authorConstants",
 				{
-					GET_ALL_AUTHORS_URL : "http://localhost:8080/lms/admin/readAllAuthors",
-					GET_AUTHOR_BY_PK_URL : "http://localhost:8080/lms/admin/returnAuthorObjectById/",
-					GET_AUTHOR_INIT_URL : "http://localhost:8080/lms/admin/getInitAuthor",
-					ADD_AUTHOR_URL : "http://localhost:8080/lms/admin/addAuthor",
-					UPDATE_AUTHOR_URL : "http://localhost:8080/lms/admin/updateAuthor",
-					DELETE_AUTHOR_URL : "http://localhost:8080/lms/admin/deleteAuthor",
-					SEARCH_AUTHORS_URL : "http://localhost:8080/lms/admin/searchByAuthorNameOnly/"
+					GET_ALL_AUTHORS_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/readAllAuthors",
+					GET_AUTHOR_BY_PK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/returnAuthorObjectById/",
+					GET_AUTHOR_INIT_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/getInitAuthor",
+					ADD_AUTHOR_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/addAuthor",
+					UPDATE_AUTHOR_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/updateAuthor",
+					DELETE_AUTHOR_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/deleteAuthor",
+					SEARCH_AUTHORS_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/searchByAuthorNameOnly/"
 				})
