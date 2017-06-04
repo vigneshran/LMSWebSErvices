88c82bd5bc87deba85f9f4bc5731e50689511ab9
vigneshran <rangaraj.vignesh@ou.edu>
2017-06-04 01:22:23 -0400

    Update publisher-constants.js

--- src/main/webapp/resources/business/AdminPublisher/publisher-constants.js
+++ src/main/webapp/resources/business/AdminPublisher/publisher-constants.js
@@ -2,11 +2,11 @@ lmsApp
 		.constant(
 				"publisherConstants",
 				{
-					GET_ALL_PUBLISHERS_URL : "http://localhost:8080/lms/admin/readAllPublishers",
-					GET_PUBLISHER_BY_PK_URL : "http://localhost:8080/lms/admin/returnPublisherObjectById/",
-					GET_PUBLISHER_INIT_URL : "http://localhost:8080/lms/admin/getInitPublisher",
-					ADD_PUBLISHER_URL : "http://localhost:8080/lms/admin/addPublisher",
-					UPDATE_PUBLISHER_URL : "http://localhost:8080/lms/admin/updatePublisher",
-					DELETE_PUBLISHER_URL : "http://localhost:8080/lms/admin/deletePublisher",
-					SEARCH_PUBLISHER_URL : "http://localhost:8080/lms/admin/searchByPublisherNameOnly/"
+					GET_ALL_PUBLISHERS_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/readAllPublishers",
+					GET_PUBLISHER_BY_PK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/returnPublisherObjectById/",
+					GET_PUBLISHER_INIT_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/getInitPublisher",
+					ADD_PUBLISHER_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/addPublisher",
+					UPDATE_PUBLISHER_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/updatePublisher",
+					DELETE_PUBLISHER_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/deletePublisher",
+					SEARCH_PUBLISHER_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/searchByPublisherNameOnly/"
 				})
