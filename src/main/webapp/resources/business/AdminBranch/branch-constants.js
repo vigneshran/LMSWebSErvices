9d47f28f9111b439f4cb844fd75e575712057647
vigneshran <rangaraj.vignesh@ou.edu>
2017-06-04 01:20:32 -0400

    Update branch-constants.js

--- src/main/webapp/resources/business/AdminBranch/branch-constants.js
+++ src/main/webapp/resources/business/AdminBranch/branch-constants.js
@@ -2,11 +2,11 @@ lmsApp
 		.constant(
 				"branchConstants",
 				{
-					GET_ALL_BRANCHES_URL : "http://localhost:8080/lms/admin/readAllBranches",
-					GET_BRANCH_BY_PK_URL : "http://localhost:8080/lms/admin/returnBranchObjectById/",
-					GET_BRANCH_INIT_URL : "http://localhost:8080/lms/admin/getInitBranch",
-					ADD_BRANCH_URL : "http://localhost:8080/lms/admin/addBranch",
-					UPDATE_BRANCH_URL : "http://localhost:8080/lms/admin/updateBranch",
-					DELETE_BRANCH_URL : "http://localhost:8080/lms/admin/deleteBranch",
-					SEARCH_BRANCHES_URL : "http://localhost:8080/lms/admin/searchByBranchNameOnly/"
+					GET_ALL_BRANCHES_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/readAllBranches",
+					GET_BRANCH_BY_PK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/returnBranchObjectById/",
+					GET_BRANCH_INIT_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/getInitBranch",
+					ADD_BRANCH_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/addBranch",
+					UPDATE_BRANCH_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/updateBranch",
+					DELETE_BRANCH_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/deleteBranch",
+					SEARCH_BRANCHES_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/searchByBranchNameOnly/"
 				})
