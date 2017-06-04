3d7357ac63adb0df707b13c7c5c40830ef58c262
vigneshran <rangaraj.vignesh@ou.edu>
2017-06-04 01:21:34 -0400

    Update override-constants.js

--- src/main/webapp/resources/business/AdminOverride/override-constants.js
+++ src/main/webapp/resources/business/AdminOverride/override-constants.js
@@ -2,10 +2,10 @@ lmsApp
 		.constant(
 				"overrideConstants",
 				{
-					GET_ALL_LOANS_DUE : "http://localhost:8080/lms/admin/readAllLoans",
-					PUSH_DUE_DATE : "http://localhost:8080/lms/admin/overrideDueDate/",
-					SEARCH_LOANS_URL : "http://localhost:8080/lms/admin/searchLoanByBorrowerNameOnly/",
-					GET_LOAN_BY_DATEOUT : "http://localhost:8080/lms/admin/getLoanByDateOut/",
-					GET_ALL_OVERDUE_LOANS : "http://localhost:8080/lms/admin/getOverDueLoans",
-					SEARCH_ALL_OVERDUE_LOANS : "http://localhost:8080/lms/admin/searchOverDueLoanByBorrowerNameOnly/"
+					GET_ALL_LOANS_DUE : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/readAllLoans",
+					PUSH_DUE_DATE : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/overrideDueDate/",
+					SEARCH_LOANS_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/searchLoanByBorrowerNameOnly/",
+					GET_LOAN_BY_DATEOUT : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/getLoanByDateOut/",
+					GET_ALL_OVERDUE_LOANS : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/getOverDueLoans",
+					SEARCH_ALL_OVERDUE_LOANS : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/searchOverDueLoanByBorrowerNameOnly/"
 				})
