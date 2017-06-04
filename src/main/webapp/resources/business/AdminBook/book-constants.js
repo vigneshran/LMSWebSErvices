4a32f22a1fcfc0631e37c113a136b9d790734211
vigneshran <rangaraj.vignesh@ou.edu>
2017-06-04 01:18:06 -0400

    Update book-constants.js

--- src/main/webapp/resources/business/AdminBook/book-constants.js
+++ src/main/webapp/resources/business/AdminBook/book-constants.js
@@ -2,14 +2,14 @@ lmsApp
 		.constant(
 				"bookConstants",
 				{
-					GET_ALL_BOOKS_URL : "http://localhost:8080/lms/admin/readAllBooks",
-					GET_ALL_AUTHORS_URL : "http://localhost:8080/lms/admin/readAllAuthors",
-					GET_ALL_PUBLISHERS_URL : "http://localhost:8080/lms/admin/readAllPublishers",
-					GET_ALL_GENRES_URL : "http://localhost:8080/lms/admin/readAllGenres",
-					GET_BOOK_BY_PK_URL : "http://localhost:8080/lms/admin/returnBookObjectById/",
-					GET_BOOK_INIT_URL : "http://localhost:8080/lms/admin/getInitBook",
-					ADD_BOOK_URL : "http://localhost:8080/lms/admin/addBook",
-					UPDATE_BOOK_URL : "http://localhost:8080/lms/admin/updateBook",
-					DELETE_BOOK_URL : "http://localhost:8080/lms/admin/deleteBook",
-					SEARCH_BOOK_URL : "http://localhost:8080/lms/admin/searchByBookNameOnly/"
+					GET_ALL_BOOKS_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/readAllBooks",
+					GET_ALL_AUTHORS_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/readAllAuthors",
+					GET_ALL_PUBLISHERS_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/readAllPublishers",
+					GET_ALL_GENRES_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/readAllGenres",
+					GET_BOOK_BY_PK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/returnBookObjectById/",
+					GET_BOOK_INIT_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/getInitBook",
+					ADD_BOOK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/addBook",
+					UPDATE_BOOK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/updateBook",
+					DELETE_BOOK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/deleteBook",
+					SEARCH_BOOK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/admin/searchByBookNameOnly/"
 				})
