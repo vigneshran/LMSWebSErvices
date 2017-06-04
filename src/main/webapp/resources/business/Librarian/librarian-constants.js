ee9b8452980d3d78483b31f6a106a96972474649
vigneshran <rangaraj.vignesh@ou.edu>
2017-06-04 01:51:07 -0400

    Update librarian-constants.js

--- src/main/webapp/resources/business/Librarian/librarian-constants.js
+++ src/main/webapp/resources/business/Librarian/librarian-constants.js
@@ -2,14 +2,14 @@ lmsApp
 		.constant(
 				"librarianConstants",
 				{
-					GET_ALL_BRANCHES_URL : "http://localhost:8080/lms/librarian/readAllBranches",
-					GET_BRANCH_BY_PK_URL : "http://localhost:8080/lms/librarian/returnBranchObjectById/",
-					GET_INIT_BRANCH : "http://localhost:8080/lms/librarian/getInitBranch",
-					GET_ALL_BOOKS_URL : "http://localhost:8080/lms/librarian/readAllBooksByBranch/",
-					GET_COPIES_URL : "http://localhost:8080/lms/librarian/getCopies/",
-					DOES_BOOK_EXIST_URL : "http://localhost:8080/lms/librarian/doesBookExist/",
-					ADD_COPIES_URL : "http://localhost:8080/lms/librarian/addCopies",
-					UPDATE_COPIES_URL : "http://localhost:8080/lms/librarian/updateCopies",
-					UPDATE_BRANCH_URL : "http://localhost:8080/lms/librarian/updateBranch",
-					SEARCH_BOOKS_URL : "http://localhost:8080/lms/librarian/searchByBookNameOnly/"
+					GET_ALL_BRANCHES_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/librarian/readAllBranches",
+					GET_BRANCH_BY_PK_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/librarian/returnBranchObjectById/",
+					GET_INIT_BRANCH : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/librarian/getInitBranch",
+					GET_ALL_BOOKS_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/librarian/readAllBooksByBranch/",
+					GET_COPIES_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/librarian/getCopies/",
+					DOES_BOOK_EXIST_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/librarian/doesBookExist/",
+					ADD_COPIES_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/librarian/addCopies",
+					UPDATE_COPIES_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/librarian/updateCopies",
+					UPDATE_BRANCH_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/librarian/updateBranch",
+					SEARCH_BOOKS_URL : "http://13.59.248.203:8080/lms-1.0.0-BUILD-SNAPSHOT/librarian/searchByBookNameOnly/"
 				})
